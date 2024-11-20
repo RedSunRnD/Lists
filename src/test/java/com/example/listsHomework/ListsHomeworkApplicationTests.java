28e4fbd275e16e46388ded681313ba6f247fdc57
RedSunRnD <vvatutin.rs@gmail.com>
2024-11-20 21:38:52 +0300

    Initial Commit

---
+++ src/test/java/com/example/listsHomework/ListsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listsHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
